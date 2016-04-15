4cac06a54d267e6367251e3e2721fc572a23efd3
Matthew Opperman <mattopp17@gmail.com>
2016-04-15 13:12:51 -0400

    Revert "give it a shot"
    This reverts commit d1ca24f206cc5e7e30a26c50b28a791bf9058f4b.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -47,6 +47,10 @@ BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x0008000 --ramdisk_offset 0x2000000
 TARGET_KERNEL_SOURCE := kernel/lge/g3
 
+ifeq ($(filter d852, $(TARGET_DEVICE)),)
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
+endif
+
 # Audio
 BOARD_USES_ALSA_AUDIO := true
 USE_CUSTOM_AUDIO_POLICY := 1
